f3cc7c2f9e57c1d7196ea4ecbd6babf010566f59
Yan qiu <yanqiu@Yans-MacBook-Air.local>
2018-10-07 00:03:11 -0700

    add

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+
+var enter=["a","b","c","d","e","f","g","h",
+           "i","j","k","l","m","n","o","p",
+           "q","r","s","t","u","v","w","x",
+           "y","z"];
+var win = 0;
+var lose = 0;
+var guessLeft = 9;
+
+var games = document.getElementById("games");
+var wins = document.getElementById("wins");
+var loses = document.getElementById("loses");
+var guessLeft = document.getElementById("guessLeft");
+var yourGuess= document.getElementById("yourGuess");
+var computerGuess = enter[Math.floor(Math.random() * enter.length)];
+
+document.onkeyup = function(event){
+   var guessValue = event.key;
+    console.log('my guess: ' + guessValue);
+    console.log('computer guess: '+ computerGuess);
+    
+   if(guessValue == computerGuess ){
+       win++;
+       console.log(win);
+       alert("You Win!");
+    
+}
+       
+   
+    else {
+        lose++;
+        console.log(lose);
+        alert("you lose!");
+    }
+
+ 
+}
+
