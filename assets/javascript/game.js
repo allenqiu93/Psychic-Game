6a01de7655897ad42c4c9da76cf1349012af1302
Yan qiu <yanqiu@Yans-MacBook-Air.local>
2018-10-15 02:20:20 -0700

    add new things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,50 @@
+var letter = ["a","b","c","d","e","f",
+              "g","h","i","j","k","l",
+              "m","n","o","p","q","r",
+              "s","t","u","v","w","x","y","z"];
 
-var enter=["a","b","c","d","e","f","g","h",
-           "i","j","k","l","m","n","o","p",
-           "q","r","s","t","u","v","w","x",
-           "y","z"];
+//wins and loses
 var win = 0;
 var lose = 0;
-var guessLeft = 9;
 
-var games = document.getElementById("games");
+var guessLeft = 9;
+var random = letter[Math.floor(Math.random()*letter.length)];
+var guessesLeft = document.getElementById("guessesLeft")
+
+//shows in html
 var wins = document.getElementById("wins");
 var loses = document.getElementById("loses");
-var guessLeft = document.getElementById("guessLeft");
-var yourGuess= document.getElementById("yourGuess");
-var computerGuess = enter[Math.floor(Math.random() * enter.length)];
+var input = document.getElementById("yourGuessInput");
 
-document.onkeyup = function(event){
-    win;
-    lose;
-    guessLeft;
+//shows player answer in console
+console.log(random);
 
-   var guessValue = event.key;
-    console.log('my guess: ' + guessValue);
-    console.log('computer guess: '+ computerGuess);
-    
-   if(guessValue == computerGuess ){
-       win++;
-       console.log(win);
-       alert("You Win!");
 
-    }
-       
-   
-    else {
-        lose++;
-        console.log(lose);
-        alert("you lose!");
-        guessLeft--;
-
-    }
-wins.textContent = "Wins: "+ win;
-loses.textContent = "Loses: "+ lose;
-yourLeft.textContent ="Guess Left: "+ guessLeft;
+function restart(){
+    guessLeft = 9;
+    guessesLeft.textContent = ("Guess Left: "+ guessLeft);
+    random = letter[Math.floor(Math.random()*letter.length)];
+    //shows player new answer in alert
+    alert(random);
 }
 
+document.onkeyup = function(event){
+    var yourGuess = event.key;
+
+    if(yourGuess === random){
+        alert("You win!");
+        win++;
+        wins.textContent = ("Wins: "+ win);
+        restart();
+    }
+
+    else  {
+        alert("Wrong guess, you lose!");
+        lose++;
+        guessLeft = guessLeft - 1;
+        guessesLeft.textContent = ("Guess Left: "+ guessLeft);
+    }
+}
+if(guessesLeft== 0){
+    restart();
+}
